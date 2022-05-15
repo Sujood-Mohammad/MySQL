14aebefdc73e463af99033b826ae85d5cc5b6eda
Sujood-Mohammad <sujoodmoh1996@gmail.com>
2022-05-16 00:22:46 +0300

    main

--- index.php
+++ index.php
@@ -16,4 +16,23 @@ if(mysqli_query($connect,$query)){
 else {
     echo "error :".mysqli_error($connect);
 }
+$select=mysqli_query( $connect,"SELECT * FROM animal");
 ?>
+<table border=1px>
+    <tr>
+        <th>Animal Name</th>
+        <th>Animal Age</th>
+        <th>Animal Price</th>
+        <th>Animal Color</th>
+        <th>Animal Sound</th>
+    </tr>
+    <?php while($row= mysqli_fetch_assoc ($select)): ?>
+    <tr>
+    <td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['age']; ?></td>
+    <td><?php echo $row['price']; ?></td>
+    <td><?php echo $row['color']; ?></td>
+    <td><?php echo $row['sound']; ?></td>
+    </tr>
+    <?php endwhile; ?>
+</table>
